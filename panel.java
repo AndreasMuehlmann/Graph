0575f8d3cec347ded3b912b48604fc23b93a13e2
Andi# <andi.muehlmann@arcor.de>
2021-11-19 23:18:02 +0100

    first commit in branch

--- panel.java
+++ panel.java
@@ -17,4 +17,18 @@ class MyPanel extends JPanel {
         // Draw Text
         g.drawString("This is my custom Panel!",10,20);
     }  
+
+    private void drawNode(Graphics2D g2d, Node node){
+        Color original = g2d.getColor();
+        g2d.drawOval(node.position[0], node.position[1], node.radius, node.radius);
+        g2d.setColor(colors[node.color]);
+        g2d.fillOval(node.position[0], node.position[1], node.radius, node.radius);
+        g2d.setColor(original);
+
+        g2d.drawString(String.valueOf(node.index), node.position[0], node.position[1]);
+    }
+
+    private void drawEdge(Graphics2D g2d, Node from, Node to){
+        g2d.drawLine(from.position[0], from.position[1], to.position[0], to.position[1]);
+    }
 }
