29937213b42f2b60033befc5c6a4f8aedcc4c343
Andi# <andi.muehlmann@arcor.de>
2021-11-19 23:15:50 +0100

    cant draw

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Main{
+    public static void main(String args[]){
+        Graph graph = new Graph(new LinkedList<Node>(), 1500, 1000);
+        graph.makeRandomNodes(2);
+    }
+}
---
+++ panel.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+class MyPanel extends JPanel {
+
+    public MyPanel() {
+        setBorder(BorderFactory.createLineBorder(Color.black));
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(1000,1000);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);       
+
+        // Draw Text
+        g.drawString("This is my custom Panel!",10,20);
+    }  
+}
